797746cc292eb34a51b739ac6873769f4e6acbc0
Draco <kevin.tenisson@gmail.com>
2023-01-02 07:04:18 -0800

    Edited cell size and added hover effect code

--- eas.js
+++ eas.js
@@ -1,14 +1,18 @@
-// Create a container element
+// Srlrct the container element for the grid
 const container = document.querySelector('#grid');
 
+// Set the container's width and height to be the same
+// (i.e. create a square grid)
+container.style.width = '800px';
+container.style.height = '800px';
 
 // Set the container's display property to 'grid'
 container.style.display = 'grid';
 
 // Set the container's grid-template-columns and grid-template-rows
 // properties to create a 16x16 grid of square cells
-container.style.gridTemplateColumns = 'repeat(16, 16px)';
-container.style.gridTemplateRows = 'repeat(16, 16px)';
+container.style.gridTemplateColumns = 'repeat(auto-fill, minmax(50px, 1fr))';
+container.style.gridTemplateRows = 'repeat(auto-fill, minmax(50px, 1fr))';
 
 
 // Create 256 div elements to fill the grid
@@ -17,4 +21,12 @@ for (let i = 0; i < 256; i++) {
   container.appendChild(cell);
 }
 
-console.log(container)
+
+// Code for hover effect
+const cells = document.querySelectorAll('#grid div');
+
+cells.forEach(cell => {
+    cell.addEventListener('mouseover', event => {
+        event.target.classList.add('hovered');
+    });
+});
