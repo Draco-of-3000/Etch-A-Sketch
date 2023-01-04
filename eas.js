11bd7608ac88d9b3443ccbafd2dd459ec52ee2e5
Draco <kevin.tenisson@gmail.com>
2023-01-04 09:21:54 -0800

    Added code to make hover RGB + made code recursive

--- eas.js
+++ eas.js
@@ -22,11 +22,6 @@ for (let i = 0; i < 256; i++) {
   container.appendChild(cell);
 }
 
-
-// Select reset and resize button
-const reset = document.querySelector('#reset');       
-const resizeButton = document.querySelector('#resize');   
-
 // Function to apply hover effect to cells
 function applyHoverEffect() {
     // Select all cells in grid
@@ -40,50 +35,91 @@ function applyHoverEffect() {
     });
 };
 
+
+// Function to apply RGB hover effect
+function hoverRGB () {
+    // Generate random values for Red, Green, and Blue (RGB)
+    const r = Math.floor(Math.random() * 256);
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
+
+    // Construct and return the random color as an RGB string
+    return `rgb(${r}, ${g}, ${b})`;
+};
+
+//Function to reset grid
+function resetGrid() {
+    // Code to remove hover effect
+    reset.addEventListener('click', () => {
+        cells.forEach(cell => {
+            cell.classList.remove('hovered');
+        });
+
+        // Reset the grid template columns and rows to create a 16x16 grid
+        container.style.gridTemplateColumns = 'repeat(16, 50px)';
+        container.style.gridTemplateRows = 'repeat(16, 50px)';
+
+        // Remove all cells from the grid
+        container.innerHTML = '';
+
+        // Create a new 16x16 grid
+        for (let i = 0; i < 256; i++) {
+            const cell = document.createElement('div');
+            container.appendChild(cell);
+            applyHoverEffect();
+        }
+    });
+};
+
+// Function to resize grid
+function resizeGrid () {
+    // Code to prompt user for squares per size
+    resizeButton.addEventListener('click', () => {
+        //Prompt user for squares per side
+        const squaresPerSide = prompt('Enter the number of squares per side for the new grid');
+        const pixelSize = 800 / squaresPerSide;
+  
+        // Update the grid template columns and rows to match the new cell size
+        container.style.gridTemplateColumns = `repeat(${squaresPerSide}, ${pixelSize}px)`;
+        container.style.gridTemplateRows = `repeat(${squaresPerSide}, ${pixelSize}px)`;
+  
+        // Create a new grid with the specified number of squares per side
+        container.innerHTML = '';
+        for (let i = 0; i < squaresPerSide ** 2; i++) {
+            const cell = document.createElement('div');
+            container.appendChild(cell);
+        }
+
+        // Re-apply hover effect to cells
+        applyHoverEffect();  
+    });
+};
+
 // Apply hover effect to cells
 applyHoverEffect();
 
-// Code to remove hover effect
-reset.addEventListener('click', () => {
-    cells.forEach(cell => {
-        cell.classList.remove('hovered');
-    });
+// When Reset button is clicked
+const reset = document.querySelector('#reset'); 
+reset.addEventListener('click', resetGrid);
 
-    // Reset the grid template columns and rows to create a 16x16 grid
-    container.style.gridTemplateColumns = 'repeat(16, 50px)';
-    container.style.gridTemplateRows = 'repeat(16, 50px)';
+// When Resize button is clicked
+const resizeButton = document.querySelector('#resize');
+resizeButton.addEventListener('click', resizeGrid);
 
-    // Remove all cells from the grid
-    container.innerHTML = '';
 
-    // Create a new 16x16 grid
-    for (let i = 0; i < 256; i++) {
-        const cell = document.createElement('div');
-        container.appendChild(cell);
-        applyHoverEffect();
-    }
-});
 
-// Code to prompt user for squares per size
-resizeButton.addEventListener('click', () => {
-    //Prompt user for squares per side
-    const squaresPerSide = prompt('Enter the number of squares per side for the new grid');
-    const pixelSize = 800 / squaresPerSide;
-  
-    // Update the grid template columns and rows to match the new cell size
-    container.style.gridTemplateColumns = `repeat(${squaresPerSide}, ${pixelSize}px)`;
-    container.style.gridTemplateRows = `repeat(${squaresPerSide}, ${pixelSize}px)`;
-  
-    // Create a new grid with the specified number of squares per side
-    container.innerHTML = '';
-    for (let i = 0; i < squaresPerSide ** 2; i++) {
-      const cell = document.createElement('div');
-      container.appendChild(cell);
-    }
 
-    // Re-apply hover effect to cells
-    applyHoverEffect();
-   
-    
-});
+
+
+
+
+
+
+
+
+
+
+
+
+
 
