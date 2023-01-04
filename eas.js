5270b7c80e4d8215eda1a5852a4360cd0c4f1889
Draco <kevin.tenisson@gmail.com>
2023-01-04 03:42:13 -0800

    added more comments

--- eas.js
+++ eas.js
@@ -1,4 +1,4 @@
-// Srlrct the container element for the grid
+// Select the container element for the grid
 const container = document.querySelector('#grid');
 
 // Set the container's width and height to be the same
@@ -11,8 +11,8 @@ container.style.display = 'grid';
 
 // Set the container's grid-template-columns and grid-template-rows
 // properties to create a 16x16 grid of square cells
-container.style.gridTemplateColumns = 'repeat(auto-fill, minmax(50px, 1fr))';
-container.style.gridTemplateRows = 'repeat(auto-fill, minmax(50px, 1fr))';
+container.style.gridTemplateColumns = 'repeat(16, 50px)';
+container.style.gridTemplateRows = 'repeat(16, 50px)';
 
 
 // Create 256 div elements to fill the grid
@@ -23,10 +23,22 @@ for (let i = 0; i < 256; i++) {
 
 
 // Code for hover effect
-const cells = document.querySelectorAll('#grid div');
+const cells = document.querySelectorAll('#grid div'); // Select all cells in grid 
+const reset = document.querySelector('#reset');       // Select reset button
+const resizeButton = document.querySelector('#resize');     // Select resize button
 
 cells.forEach(cell => {
     cell.addEventListener('mouseover', event => {
-        event.target.classList.add('hovered');
+        event.target.classList.add('hovered'); // Add hovered class to cells in when hovered upon
     });
 });
+
+// Code to remove hover effect
+reset.addEventListener('click', () => {
+    cells.forEach(cell => {
+        cell.classList.remove('hovered');
+    });
+});
+
+
+
